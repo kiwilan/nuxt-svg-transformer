36ed931577c7ae64dfb38caf1295e65067e7f007
Ewilan Rivière <ewilan.riviere@gmail.com>
2023-02-10 19:08:56 +0100

    fix ci

--- playground/src/app.vue
+++ playground/src/app.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { onMounted, ref } from 'vue'
+import { ref } from 'vue'
 import type { IconType } from '~~/.nuxt/svg-transformer'
 
 const like = ref<IconType>('heart')
